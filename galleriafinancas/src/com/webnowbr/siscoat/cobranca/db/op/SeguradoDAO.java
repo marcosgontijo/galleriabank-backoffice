9a2cbcd89046fab7359f714c35cf2e0d99ce5ded
Alexandre Murta <amurta@gmail.com>
2021-09-03 17:35:05 -0300

    #SIS-110# Acerto no cnpj do segurado

--- galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/op/SeguradoDAO.java
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/op/SeguradoDAO.java
@@ -19,7 +19,7 @@ import com.webnowbr.siscoat.seguro.vo.SeguroTabelaVO;
 public class SeguradoDAO extends HibernateDao <Segurado,Long> {
 	
 	
-	private static final String QUERY_SEGURDOS_DFI = "select coco.numerocontrato,  datacontrato, numerocontratoseguro, valorimovel, pare.cpf, pare.nome, segu.porcentagemsegurador, " +
+	private static final String QUERY_SEGURDOS_DFI = "select coco.numerocontrato,  datacontrato, numerocontratoseguro, valorimovel, pare.cpf, pare.cnpj, pare.nome, segu.porcentagemsegurador, " +
 		      "coco.qtdeparcelas, count( ccd.id  ) qtdeparcelasFaltantes, pare.endereco, pare.numero, pare.complemento, pare.bairro, pare.cidade, pare.estado, pare.cep  " +
 		       "from cobranca.contratocobranca coco " +
 		       "inner join cobranca.segurado segu on coco.id = segu.contratocobranca " +
@@ -27,10 +27,10 @@ public class SeguradoDAO extends HibernateDao <Segurado,Long> {
 		       "inner join cobranca.contratocobranca_detalhes_join ccdj ON ccdj.idcontratocobranca = coco.id " +
 		       "left join cobranca.contratocobrancadetalhes ccd ON ccd.id = ccdj.idcontratocobrancadetalhes and ccd.parcelapaga = false " +
 		       "where coco.temsegurodfi = true and to_char(ccd.dataVencimento, 'YYYYMM') = ? " +
-		       "group by coco.numerocontrato,  datacontrato, numerocontratoseguro, valorimovel, pare.cpf, pare.nome, segu.porcentagemsegurador, " +
+		       "group by coco.numerocontrato,  datacontrato, numerocontratoseguro, valorimovel, pare.cpf, pare.cnpj, pare.nome, segu.porcentagemsegurador, " +
 		       "coco.qtdeparcelas, pare.endereco, pare.numero, pare.complemento, pare.bairro, pare.cidade, pare.estado, pare.cep " ;
 	
-	private static final String QUERY_SEGURDOS_MIP = "select coco.numerocontrato,  datacontrato, numerocontratoseguro, sum(ccd.vlrparcela) saldodevedor, pare.cpf, pare.nome, \r\n"
+	private static final String QUERY_SEGURDOS_MIP = "select coco.numerocontrato,  datacontrato, numerocontratoseguro, sum(ccd.vlrparcela) saldodevedor, pare.cpf, pare.cnpj, pare.nome, \r\n"
 			+ "	segu.porcentagemsegurador, coco.qtdeparcelas, count( ccd.id  ) qtdeparcelasFaltantes, pare.endereco, pare.numero, \r\n"
 			+ "	pare.complemento, pare.bairro, pare.cidade, pare.estado, pare.cep, pare.dtnascimento, pare.sexo \r\n"
 			+ "from cobranca.contratocobranca coco\r\n"
@@ -39,7 +39,7 @@ public class SeguradoDAO extends HibernateDao <Segurado,Long> {
 			+ "inner join cobranca.contratocobranca_detalhes_join ccdj ON ccdj.idcontratocobranca = coco.id\r\n"
 			+ "left join cobranca.contratocobrancadetalhes ccd ON ccd.id = ccdj.idcontratocobrancadetalhes and ccd.parcelapaga = false\r\n"
 			+ "where coco.temseguromip = true  and to_char(ccd.dataVencimento, 'YYYYMM') = ? \r\n"
-			+ "group by coco.numerocontrato,  datacontrato, numerocontratoseguro, pare.cpf, pare.nome, segu.porcentagemsegurador,\r\n"
+			+ "group by coco.numerocontrato,  datacontrato, numerocontratoseguro, pare.cpf, pare.cnpj, pare.nome, segu.porcentagemsegurador,\r\n"
 			+ "coco.qtdeparcelas, pare.endereco, pare.numero, pare.complemento, pare.bairro, pare.cidade, pare.estado, pare.cep, \r\n"
 			+ "pare.dtnascimento, pare.sexo" ;
 	
@@ -87,7 +87,11 @@ public class SeguradoDAO extends HibernateDao <Segurado,Long> {
 							seguroTabelaVO.setParcelasOriginais(rs.getString("qtdeparcelas"));
 							seguroTabelaVO.setParcelasFaltantes(rs.getString("qtdeparcelasFaltantes"));
 							seguroTabelaVO.setPorcentagemPrincipal(rs.getBigDecimal("porcentagemsegurador"));
-							seguroTabelaVO.setCpfPrincipal(rs.getString("cpf"));
+							if ( !CommonsUtil.semValor(rs.getString("cpf")) ) {
+								seguroTabelaVO.setCpfPrincipal(rs.getString("cpf"));
+							}else {
+								seguroTabelaVO.setCpfPrincipal(rs.getString("cnpj"));
+							}
 							seguroTabelaVO.setNomePrincipal(rs.getString("nome"));
 							seguroTabelaVO.setLogradouro(rs.getString("endereco"));
 							seguroTabelaVO.setNumeroResidencia(rs.getString("numero"));
@@ -96,18 +100,31 @@ public class SeguradoDAO extends HibernateDao <Segurado,Long> {
 							seguroTabelaVO.setCidade(rs.getString("cidade"));
 							seguroTabelaVO.setUf(rs.getString("estado"));
 							seguroTabelaVO.setCep(rs.getString("cep"));						
-							numeroContratoSeguroantigo = (rs.getString("numerocontratoseguro"));						
+							numeroContratoSeguroantigo = (rs.getString("numerocontratoseguro"));
+							
 						} else if (CommonsUtil.semValor(seguroTabelaVO.getPorcentagem2()) && CommonsUtil.semValor(seguroTabelaVO.getCpf2()) && CommonsUtil.semValor(seguroTabelaVO.getNome2())) {
-							seguroTabelaVO.setPorcentagem2(rs.getBigDecimal("porcentagemsegurador"));
-							seguroTabelaVO.setCpf2(rs.getString("cpf"));
+							seguroTabelaVO.setPorcentagem2(rs.getBigDecimal("porcentagemsegurador"));							
+							if ( !CommonsUtil.semValor(rs.getString("cpf")) ) {
+								seguroTabelaVO.setCpf2(rs.getString("cpf"));
+							}else {
+								seguroTabelaVO.setCpf2(rs.getString("cnpj"));
+							}
 							seguroTabelaVO.setNome2(rs.getString("nome"));							
 						}  else if (CommonsUtil.semValor(seguroTabelaVO.getPorcentagem3()) && CommonsUtil.semValor(seguroTabelaVO.getCpf3()) && CommonsUtil.semValor(seguroTabelaVO.getNome3())) {
 							seguroTabelaVO.setPorcentagem3(rs.getBigDecimal("porcentagemsegurador"));
-							seguroTabelaVO.setCpf3(rs.getString("cpf"));
+							if ( !CommonsUtil.semValor(rs.getString("cpf")) ) {
+								seguroTabelaVO.setCpf3(rs.getString("cpf"));
+							}else {
+								seguroTabelaVO.setCpf3(rs.getString("cnpj"));
+							}							
 							seguroTabelaVO.setNome3(rs.getString("nome"));							
 						} else {
 							seguroTabelaVO.setPorcentagem4(rs.getBigDecimal("porcentagemsegurador"));
-							seguroTabelaVO.setCpf4(rs.getString("cpf"));
+							if ( !CommonsUtil.semValor(rs.getString("cpf")) ) {
+								seguroTabelaVO.setCpf4(rs.getString("cpf"));
+							}else {
+								seguroTabelaVO.setCpf4(rs.getString("cnpj"));
+							}
 							seguroTabelaVO.setNome4(rs.getString("nome"));
 						}
 																		
