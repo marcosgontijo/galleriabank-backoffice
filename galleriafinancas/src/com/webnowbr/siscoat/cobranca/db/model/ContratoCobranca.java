a6a0efb87b6eca938ec0ac6f9da60eaa1f0ec5ea
Hermes Jr <webnowbr@gmail.com>
2021-11-03 15:49:31 -0300

    Atualizações

--- galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/model/ContratoCobranca.java
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/model/ContratoCobranca.java
@@ -2,6 +2,7 @@ package com.webnowbr.siscoat.cobranca.db.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.math.BigInteger; 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
