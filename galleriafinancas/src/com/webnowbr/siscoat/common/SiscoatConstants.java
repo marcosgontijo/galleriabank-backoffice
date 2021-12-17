93aa87b49695f99e18632489d1991d6da61804eb
Alexandre Murta <amurta@gmail.com>
2021-12-17 14:20:44 -0300

    Acerto no simulador, custo de emissao valor liquido e bruto

--- galleriafinancas/src/com/webnowbr/siscoat/common/SiscoatConstants.java
+++ galleriafinancas/src/com/webnowbr/siscoat/common/SiscoatConstants.java
@@ -14,7 +14,8 @@ public class SiscoatConstants {
 	public static final BigDecimal SEGURO_MIP = BigDecimal.valueOf(0.021); // seguro MIP: 0,021% sobre saldo devedor parcela anterior;
 	public static final BigDecimal SEGURO_DFI = BigDecimal.valueOf(0.0037); // seguro DFI: 0,0037% sobre o valor do imóvel;
 
-	public static final BigDecimal CUSTO_EMISSAO_PERCENTUAL = BigDecimal.valueOf(3.2);
+	public static final BigDecimal CUSTO_EMISSAO_PERCENTUAL_LIQUIDO = BigDecimal.valueOf(3.2);
+	public static final BigDecimal CUSTO_EMISSAO_PERCENTUAL_BRUTO = BigDecimal.valueOf(3);
 	public static final BigDecimal CUSTO_EMISSAO_MINIMO 	 = BigDecimal.valueOf(3500);
 	
 	@SuppressWarnings("deprecation")
