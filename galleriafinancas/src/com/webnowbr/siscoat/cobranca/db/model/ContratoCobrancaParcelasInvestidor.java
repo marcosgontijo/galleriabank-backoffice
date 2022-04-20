0dcf88069b841563998dc7ac6fc595e6da70b33b
Lucas <lucaslamr@gmail.com>
2022-04-20 17:47:42 -0300

    atualizaçao

--- galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/model/ContratoCobrancaParcelasInvestidor.java
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/model/ContratoCobrancaParcelasInvestidor.java
@@ -70,7 +70,14 @@ public class ContratoCobrancaParcelasInvestidor implements Serializable {
 			this.valorLiquido = valorLiquidoBaixa;
 		}
 	}
-
+	
+	public void setJurosBaixa(BigDecimal jurosBaixa) {
+		if (BigDecimal.ZERO.compareTo(CommonsUtil.bigDecimalValue(capitalizacao)) == -1) {
+			this.capitalizacao = jurosBaixa;
+		} else {
+			this.juros = jurosBaixa;
+		}
+	}
 
 	public BigDecimal getParcelaMensalBaixa() {		
 		if (BigDecimal.ZERO.compareTo(CommonsUtil.bigDecimalValue(capitalizacao)) == -1) {
@@ -80,15 +87,6 @@ public class ContratoCobrancaParcelasInvestidor implements Serializable {
 		}
 		return parcelaMensalBaixa;
 	}
-
-	public void setJurosBaixa(BigDecimal jurosBaixa) {
-		if (BigDecimal.ZERO.compareTo(CommonsUtil.bigDecimalValue(capitalizacao)) == -1) {
-			this.capitalizacao = jurosBaixa;
-		} else {
-			this.juros = jurosBaixa;
-		}
-	}
-
 
 	public BigDecimal getJurosBaixa() {
 		if (BigDecimal.ZERO.compareTo(CommonsUtil.bigDecimalValue(capitalizacao)) == -1) {
