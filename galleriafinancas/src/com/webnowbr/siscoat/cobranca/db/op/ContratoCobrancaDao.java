a389b82e336db4d81d2ff1b4499fcd8f56445d74
Lucas <lucaslamr@gmail.com>
2022-05-06 11:58:19 -0300

    atualizacao validacao imovel

--- galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/op/ContratoCobrancaDao.java
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/op/ContratoCobrancaDao.java
@@ -6648,7 +6648,8 @@ public class ContratoCobrancaDao extends HibernateDao <ContratoCobranca,Long> {
 				try {		
 					String query = QUERY_CONTRATOS_PENDENTES_VALIDACAO_NOVO_CONTRATO;
 					
-					query = query + "where status != 'Aprovado' and status != 'Reprovado' and status != 'Desistência Cliente' and status != 'Baixado'" ;
+					//query = query + "where status != 'Aprovado' and status != 'Reprovado' and status != 'Desistência Cliente' and status != 'Baixado'" ;
+					query = query + "where status != 'Aprovado' " ;
 					
 					connection = getConnection();
 					
