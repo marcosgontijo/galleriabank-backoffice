fc8508c2aa0a7e02b0e250508c607baef2274080
Hermes Vieira Junior <hermesjr@MBP-de-Hermes.lan>
2022-08-03 16:07:42 -0300

    Atualizações

--- galleriafinancas/src/com/webnowbr/siscoat/common/SiscoatConstants.java
+++ galleriafinancas/src/com/webnowbr/siscoat/common/SiscoatConstants.java
@@ -14,7 +14,8 @@ public class SiscoatConstants {
 	public static final BigDecimal SEGURO_MIP = BigDecimal.valueOf(0.021); // seguro MIP: 0,021% sobre saldo devedor parcela anterior;
 	public static final BigDecimal SEGURO_DFI = BigDecimal.valueOf(0.0037); // seguro DFI: 0,0037% sobre o valor do imóvel;
 	public static final BigDecimal SEGURO_DFI_6_DIGITOS = BigDecimal.valueOf(0.000037); // seguro DFI: 0,0037% sobre o valor do imóvel;
-
+	public static final BigDecimal SEGURO_MIP_5_DIGITOS = BigDecimal.valueOf(0.00021); // seguro MIP: 0,021% sobre saldo devedor parcela anterior;
+	
 	public static final BigDecimal CUSTO_EMISSAO_PERCENTUAL_LIQUIDO = BigDecimal.valueOf(3.2);
 	public static final BigDecimal CUSTO_EMISSAO_PERCENTUAL_BRUTO = BigDecimal.valueOf(3);
 	public static final BigDecimal CUSTO_EMISSAO_MINIMO 	 = BigDecimal.valueOf(3500);
