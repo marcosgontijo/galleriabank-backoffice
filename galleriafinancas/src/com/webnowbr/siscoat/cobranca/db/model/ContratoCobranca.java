29bcbbc20faa90a337505899c7667437bad6d2a0
Alexandre Murta <amurta@gmail.com>
2023-05-16 23:58:13 -0300

    acerto condicao pre analise

--- galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/model/ContratoCobranca.java
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/model/ContratoCobranca.java
@@ -978,7 +978,9 @@ public class ContratoCobranca implements Serializable {
 	public boolean isEmAnalise() {
 		List<String> lstEmAnalise =  Arrays.asList("Pendente");
 		return CommonsUtil.semValor(this.cadastroAprovadoValor) && lstEmAnalise.contains(this.status);
-	}	
+	}
+	
+	
 
 	/**
 	 * @return the id
