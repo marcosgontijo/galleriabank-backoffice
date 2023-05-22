89a9b98a1ae83842c5ebde005428efa64fcc987b
Alexandre Murta <amurta@gmail.com>
2023-05-22 09:34:56 -0300

    Criando classe para resumo das consultas analisadas

---
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/model/DocumentoAnaliseResumo.java
@@ -0,0 +1,35 @@
+package com.webnowbr.siscoat.cobranca.db.model;
+
+import java.io.Serializable;
+
+import com.webnowbr.siscoat.common.CommonsUtil;
+import com.webnowbr.siscoat.common.DocumentosAnaliseEnum;
+
+public class DocumentoAnaliseResumo implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5727133605918684738L;
+
+	private String descricao;
+
+	private String valor;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+}
