7918f56ee6f9c328b31b3d02afe05f4c52cfb03d
Alexandre Murta <amurta@gmail.com>
2023-05-24 10:41:44 -0300

    Criando consultas por pagador recebedor

---
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/db/op/PagadorRecebedorConsultaDao.java
@@ -0,0 +1,58 @@
+package com.webnowbr.siscoat.cobranca.db.op;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.webnowbr.siscoat.cobranca.db.model.PagadorRecebedor;
+import com.webnowbr.siscoat.cobranca.db.model.PagadorRecebedorConsulta;
+import com.webnowbr.siscoat.common.DocumentosAnaliseEnum;
+import com.webnowbr.siscoat.db.dao.*;
+
+/**
+ * DAO access layer for the Tecnico entity
+ * 
+ * @author hv.junior
+ *
+ */
+public class PagadorRecebedorConsultaDao extends HibernateDao<PagadorRecebedorConsulta, Long> {
+
+	private static final String QUERY_PESQUISA_CONSULTA = "select id from cobranca.pagadorrecebedorconsulta "
+			+ "where pessoa = ? and tipo = ? ";
+
+	@SuppressWarnings("unchecked")
+	public PagadorRecebedorConsulta getConsultaByPagadorAndTipo(final PagadorRecebedor pagador,
+			final DocumentosAnaliseEnum tipoConsulta) {
+		return (PagadorRecebedorConsulta) executeDBOperation(new DBRunnable() {
+			@Override
+			public Object run() throws Exception {
+				PagadorRecebedorConsulta pagadorRecebedor = new PagadorRecebedorConsulta();
+
+				Connection connection = null;
+				PreparedStatement ps = null;
+				ResultSet rs = null;
+				try {
+					connection = getConnection();
+
+					ps = connection.prepareStatement(QUERY_PESQUISA_CONSULTA);
+
+					ps.setLong(1, pagador.getId());
+					ps.setString(2, tipoConsulta.getNome());
+
+					rs = ps.executeQuery();
+
+					if (rs.next()) {
+						pagadorRecebedor = findById(rs.getLong(1));
+					}
+
+				} finally {
+					closeResources(connection, ps, rs);
+				}
+				return pagadorRecebedor;
+			}
+		});
+	}
+	
+}
