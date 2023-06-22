fa9b77b251041b49a24a62d7fabb8fb07e7f7d5a
Alexandre Murta <amurta@gmail.com>
2023-06-22 12:14:40 -0300

    Serasa consulta avulsa

--- galleriafinancas/src/com/webnowbr/siscoat/cobranca/service/SerasaService.java
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/service/SerasaService.java
@@ -103,8 +103,7 @@ public class SerasaService {
 
 			}
 
-		} catch (
-		MalformedURLException e) {
+		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -118,6 +117,10 @@ public class SerasaService {
 	private String executaConsultaSerasa(String tipoPessoa, String cnpjcpf)
 			throws MalformedURLException, IOException, ProtocolException, UnsupportedEncodingException {
 
+		
+		return "{\"consDtCadastro\":null,\"consCdCnpjcpf\":null,\"consDtConsulta\":\"2023-06-12T17:49:10.000-03:00\",\"consResultadoConsulta\":null,\"pessoa\":{\"pessTpFisicaJuridica\":\"F\",\"cnpjcpf\":\"02481669053\",\"nomeRazaoSocial\":\"YORK MOREIRA ANGELO\",\"dataNascimentoFundacao\":\"1930-05-12T00:00:00.000-03:00\",\"pessCdSituacao\":\"7\",\"descricaoSituacao\":\"TITULAR FALECIDO\",\"dataSituacaoDocumento\":\"2022-09-30T00:00:00.000-03:00\",\"nomeMae\":\"CELINA MOREIRA ANGELO\"},\"codigoServicoConsultaCredito\":null,\"codigoServicoConsultaCreditoSituacao\":null,\"htmlRelatorioBoaVistaPF\":null,\"relatorioHtml\":null,\"cmc7\":null,\"dataEmissaoCheque\":null,\"valorCheque\":null,\"qtdeCheque\":null,\"telefoneCheque\":null,\"cepCheque\":null,\"tipoConsultaCheque\":null,\"codigoDocumentoBanisys\":null,\"flagInformacaoEleitoral\":null,\"flagExtraAcao\":null,\"flagExtraPendencia\":null,\"flagExtraProtesto\":null,\"cepPesquisado\":null,\"featuresAdicionais\":null,\"flagFeaturesAdicionais\":null,\"tipoTratamentoTelefone\":\"S\",\"consultas\":[{\"codigoPessoaConsultaCrednet\":null,\"tipoRegistro\":null,\"subtipoRegistro\":null,\"mensagem\":null,\"qtdeConsultaUltimo15dias\":null,\"qtdeConsultaUltimo30dias\":null,\"qtdeConsultaUltimo31e60dias\":null,\"qtdeConsultaUltimo61e90dias\":null}],\"rendaEstimada\":{\"subTipo\":null,\"mensagem\":null,\"renda\":1450},\"capacidadePagamento\":{\"subTipo\":null,\"mensagem\":null,\"valor\":450},\"comprometimentoRenda\":{\"subTipo\":null,\"mensagem\":null,\"percentualComprometimento\":70},\"scorePostivo\":{\"tipo\":null,\"score\":497,\"range\":\"F\",\"taxa\":28,\"mensagem\":\"ESPACO RESERVADO PARA MENSAGEM DA INSTITUICAO\",\"codigoMensagem\":\"\"},\"participacoes\":[],\"alertasDocumento\":{\"subTipoAlerta\":null,\"codigoMensagem\":null,\"totalMensagem\":null,\"tipoDocumento\":null,\"numeroDocumento\":null,\"codigoMotivoOcorrencia\":null,\"dataOcorrencia\":null,\"codigoDDDTelefone1\":null,\"numeroTelefone1\":null,\"codigoDDDTelefone2\":null,\"numeroTelefone2\":null,\"codigoDDDTelefone3\":null,\"numeroTelefone3\":null,\"mensagem\":null,\"listaAlertaDocumentoDetalhes\":[]},\"pendenciasFinanceiras\":[],\"protesto\":null,\"chequeSemFundo\":null,\"acaoJudicial\":null,\"habitoPagamentos\":[{\"sequenciaEnviada\":1,\"feature\":\"REIV\",\"subTipo\":\"99\",\"faixaDias\":\"NAO HA\",\"nota\":\"HI\",\"faixaPgto\":\"STORICO\"}],\"indiceRelacionamentos\":[{\"sequenciaEnviada\":null,\"faixa\":\"SR\",\"calculado\":\"S\",\"mensagem\":\"SEM RELACIONAMENTO\",\"setor\":\"01\",\"relacionamento\":\"Sem relacionamento\",\"tendencia\":\"Sem relacionamento\"},{\"sequenciaEnviada\":null,\"faixa\":\"B2\",\"calculado\":\"S\",\"mensagem\":\"BAIXO RELACIONAMENTO COM O MERCADO COM TENDENCIA ESTAVEL\",\"setor\":\"02\",\"relacionamento\":\"Baixo relacionamento\",\"tendencia\":\"Tendência estável\"},{\"sequenciaEnviada\":null,\"faixa\":\"SR\",\"calculado\":\"S\",\"mensagem\":\"SEM RELACIONAMENTO\",\"setor\":\"03\",\"relacionamento\":\"Sem relacionamento\",\"tendencia\":\"Sem relacionamento\"},{\"sequenciaEnviada\":null,\"faixa\":\"B2\",\"calculado\":\"S\",\"mensagem\":\"BAIXO RELACIONAMENTO COM O MERCADO COM TENDENCIA ESTAVEL\",\"setor\":\"99\",\"relacionamento\":\"Baixo relacionamento\",\"tendencia\":\"Tendência estável\"}],\"obitos\":[{\"mensagem\":\"ULTIMA ATUALIZACAO DAS INFORMACOES EM 02/05/2021\"},{\"mensagem\":\"NOME DO TITULAR: YORK MOREIRA ANGELO\"},{\"mensagem\":\"DATA DE NASCIMENTO: 12/05/1930\"},{\"mensagem\":\"ANO DO OBITO: 2015\"},{\"mensagem\":\"NOME DA MAE: CELINA MOREIRA ANGELO\"}],\"relatorioPdf\":null,\"pefin\":[],\"refin\":[],\"dividaVencida\":[],\"pefinResumo\":null,\"refinResumo\":null,\"dividaVencidaResumo\":null,\"chequeSemFundoLista\":[],\"protestoLista\":[],\"alertasDocumentoLista\":[],\"acoesCivil\":null,\"falencias\":null,\"falenciasInsucesso\":null,\"relacionamentoMercado\":{\"sequenciaEnviada\":null,\"faixa\":\"B2\",\"calculado\":\"S\",\"mensagem\":\"BAIXO RELACIONAMENTO COM O MERCADO COM TENDENCIA ESTAVEL\",\"setor\":\"99\",\"relacionamento\":\"Baixo relacionamento\",\"tendencia\":\"Tendência estável\"},\"relacionamentoBanco\":{\"sequenciaEnviada\":null,\"faixa\":\"SR\",\"calculado\":\"S\",\"mensagem\":\"SEM RELACIONAMENTO\",\"setor\":\"01\",\"relacionamento\":\"Sem relacionamento\",\"tendencia\":\"Sem relacionamento\"},\"relacionamentoTelecomunicacoes\":{\"sequenciaEnviada\":null,\"faixa\":\"B2\",\"calculado\":\"S\",\"mensagem\":\"BAIXO RELACIONAMENTO COM O MERCADO COM TENDENCIA ESTAVEL\",\"setor\":\"02\",\"relacionamento\":\"Baixo relacionamento\",\"tendencia\":\"Tendência estável\"},\"relacionamentoOutrosSetores\":{\"sequenciaEnviada\":null,\"faixa\":\"SR\",\"calculado\":\"S\",\"mensagem\":\"SEM RELACIONAMENTO\",\"setor\":\"03\",\"relacionamento\":\"Sem relacionamento\",\"tendencia\":\"Sem relacionamento\"},\"possuiHabitoPagamentos\":false,\"habitoPagamentosPontual\":null,\"habitoPagamentos001_007\":null,\"habitoPagamentos008_014\":null,\"habitoPagamentos015_020\":null,\"habitoPagamentos021_030\":null,\"habitoPagamentosMais30\":null,\"habitoPagamentosMais60\":null}";
+		
+		/*
 		int HTTP_COD_SUCESSO = 200;
 
 		String retornoConsulta;
@@ -158,14 +161,13 @@ public class SerasaService {
 
 		}
 		myURLConnection.disconnect();
-		return retornoConsulta;
+		return retornoConsulta;*/
 	}
 
 	public String serasaCriarConsulta(String scnpjCpf) {
 		try {
 			return executaConsultaSerasa(CommonsUtil.pessoaFisicaJuridicaCnpjCpf(scnpjCpf), scnpjCpf);
-		} catch (
-		MalformedURLException e) {
+		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -177,56 +179,7 @@ public class SerasaService {
 
 	public String baixarDocumento(DocumentoAnalise documentoAnalise) { // POST para gerar consulta
 		try {
-			String base64 = null;
-			// loginDocket();
-			int HTTP_COD_SUCESSO = 200;
-			int HTTP_COD_SUCESSO2 = 201;
-
-			URL myURL;
-			if (CommonsUtil.mesmoValor("PF", documentoAnalise.getTipoPessoa()))
-				myURL = new URL("https://servicos.galleriabank.com.br/crednet/api/v1/");
-			else
-				myURL = new URL("https://servicos.galleriabank.com.br/relato/api/v1/");
-
-			HttpURLConnection myURLConnection = (HttpURLConnection) myURL.openConnection();
-			myURLConnection.setRequestMethod("POST");
-			myURLConnection.setUseCaches(false);
-			myURLConnection.setRequestProperty("Accept", "application/json");
-			myURLConnection.setRequestProperty("Accept-Charset", "utf-8");
-			myURLConnection.setRequestProperty("Content-Type", "application/json");
-			myURLConnection.setRequestProperty("Authorization",
-					"Bearer " + br.com.galleriabank.jwt.common.JwtUtil.generateJWTServicos());
-			myURLConnection.setDoOutput(true);
-
-//			JSONObject jsonWhatsApp = engineBodyJsonEngine(engine.getPagador());
-
-			try (OutputStream os = myURLConnection.getOutputStream()) {
-				byte[] input = documentoAnalise.getRetornoSerasa().getBytes("utf-8");
-				os.write(input, 0, input.length);
-			}
-
-			FacesMessage result = null;
-			if (myURLConnection.getResponseCode() != HTTP_COD_SUCESSO
-					&& myURLConnection.getResponseCode() != HTTP_COD_SUCESSO2) {
-				result = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-						"Engine: Falha  (Cod: " + myURLConnection.getResponseCode() + ")", "");
-			} else {
-				// docket = new Docket(objetoContratoCobranca, listaPagador, estadoImovel, "" ,
-				// cidadeImovel, "", getNomeUsuarioLogado(), gerarDataHoje());
-				result = new FacesMessage(FacesMessage.SEVERITY_INFO, "Consulta feita com sucesso", "");
-				BufferedReader in;
-				in = new BufferedReader(new InputStreamReader(myURLConnection.getInputStream(), "UTF-8"));
-				String inputLine;
-				StringBuffer response = new StringBuffer();
-				while ((inputLine = in.readLine()) != null) {
-					response.append(inputLine);
-				}
-				base64 = response.toString();
-				in.close();
-			}
-
-			myURLConnection.disconnect();
-			return base64;
+			return baixarDocumentoConsulta(documentoAnalise.getRetornoSerasa(), documentoAnalise.getTipoPessoa());
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -236,6 +189,73 @@ public class SerasaService {
 		}
 
 		return null;
+	}
+	
+	public String baixarDocumento(String scnpjCpf, String retornoSerasa) {
+		try {
+			return baixarDocumentoConsulta(CommonsUtil.pessoaFisicaJuridicaCnpjCpf(scnpjCpf), scnpjCpf);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private String baixarDocumentoConsulta(String retornoSerasa, String tipoPessoa)
+			throws MalformedURLException, IOException, ProtocolException, UnsupportedEncodingException {
+		String base64 = null;
+		// loginDocket();
+		int HTTP_COD_SUCESSO = 200;
+		int HTTP_COD_SUCESSO2 = 201;
+
+		URL myURL;
+		if (CommonsUtil.mesmoValor("PF", tipoPessoa))
+			myURL = new URL("https://servicos.galleriabank.com.br/crednet/api/v1/");
+		else
+			myURL = new URL("https://servicos.galleriabank.com.br/relato/api/v1/");
+
+		HttpURLConnection myURLConnection = (HttpURLConnection) myURL.openConnection();
+		myURLConnection.setRequestMethod("POST");
+		myURLConnection.setUseCaches(false);
+		myURLConnection.setRequestProperty("Accept", "application/json");
+		myURLConnection.setRequestProperty("Accept-Charset", "utf-8");
+		myURLConnection.setRequestProperty("Content-Type", "application/json");
+		myURLConnection.setRequestProperty("Authorization",
+				"Bearer " + br.com.galleriabank.jwt.common.JwtUtil.generateJWTServicos());
+		myURLConnection.setDoOutput(true);
+
+//			JSONObject jsonWhatsApp = engineBodyJsonEngine(engine.getPagador());
+
+		try (OutputStream os = myURLConnection.getOutputStream()) {
+			byte[] input = retornoSerasa.getBytes("utf-8");
+			os.write(input, 0, input.length);
+		}
+
+		FacesMessage result = null;
+		if (myURLConnection.getResponseCode() != HTTP_COD_SUCESSO
+				&& myURLConnection.getResponseCode() != HTTP_COD_SUCESSO2) {
+			result = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					"Engine: Falha  (Cod: " + myURLConnection.getResponseCode() + ")", "");
+		} else {
+			// docket = new Docket(objetoContratoCobranca, listaPagador, estadoImovel, "" ,
+			// cidadeImovel, "", getNomeUsuarioLogado(), gerarDataHoje());
+			result = new FacesMessage(FacesMessage.SEVERITY_INFO, "Consulta feita com sucesso", "");
+			BufferedReader in;
+			in = new BufferedReader(new InputStreamReader(myURLConnection.getInputStream(), "UTF-8"));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			base64 = response.toString();
+			in.close();
+		}
+
+		myURLConnection.disconnect();
+		return base64;
 	}
 
 }
