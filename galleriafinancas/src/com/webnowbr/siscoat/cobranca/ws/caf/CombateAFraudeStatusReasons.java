72ca080ad48dec86ad4e5021eb600f7dd3b73c68
Lucaslamr <lucaslamr@gmail.com>
2023-07-06 08:54:02 -0300

    att combate a fraude

---
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/ws/caf/CombateAFraudeStatusReasons.java
@@ -0,0 +1,9 @@
+package com.webnowbr.siscoat.cobranca.ws.caf;
+
+public class CombateAFraudeStatusReasons {
+	public String category;
+	public String code;
+	public String status;
+	public String resultStatus;
+	public String description;
+}
