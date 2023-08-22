9f6fcdc0d42cb8f850860c3982683d94912e79db
Lucaslamr <lucaslamr@gmail.com>
2023-08-22 13:07:54 -0300

    atualizacoes

--- galleriafinancas/src/com/webnowbr/siscoat/cobranca/ws/caf/CombateAFraudeMB.java
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/ws/caf/CombateAFraudeMB.java
@@ -161,10 +161,8 @@ public class CombateAFraudeMB {
 			output.close();
 			facesContext.responseComplete();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
