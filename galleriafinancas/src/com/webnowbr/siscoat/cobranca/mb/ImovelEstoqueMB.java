53f347b06227ff155a5a933377a5c495d55f9632
Guilherme Gonçalves <Guilherme@GuilhermeKaju>
2023-09-08 18:02:09 -0300

    att estoque

--- galleriafinancas/src/com/webnowbr/siscoat/cobranca/mb/ImovelEstoqueMB.java
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/mb/ImovelEstoqueMB.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -16,6 +17,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
+import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.IndexedColors;
@@ -109,15 +111,22 @@ public class ImovelEstoqueMB {
 			
 			
 			if(CommonsUtil.semValor(this.objetoImovelCobranca.getImovelEstoque())) {
-				this.objetoImovelCobranca.setImovelEstoque(this.objetoImovelEstoque);
+				this.objetoImovelCobranca.setImovelEstoque(this.objetoImovelEstoque);	
 			
 			ImovelCobrancaDao imovelCobrancaDao = new ImovelCobrancaDao();				
 			imovelCobrancaDao.merge(this.objetoImovelCobranca);
 			}
 			
+			// Chama os métodos de cálculo e define os valores diretamente nos campos do objeto ImovelEstoque
+			this.objetoImovelEstoque.setVariacaoCusto(
+		            calcularVariacaoCustos(this.objetoImovelEstoque.getValorLeilao2(), this.objetoImovelEstoque.getValorEmprestimo())
+		        );
+		        this.objetoImovelEstoque.setLtvLeilao(
+		            calcularLtvLeilao(this.objetoImovelEstoque.getValorLeilao2(), this.objetoImovelEstoque.getValorMercado())
+		        );
 
-			context.addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_INFO, "Estoque Inserido com sucesso!!", ""));
+
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Estoque Inserido com sucesso!!", ""));
 			clearFieldsEstoqueImoveis();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -137,177 +146,233 @@ public class ImovelEstoqueMB {
 
 		return "/Atendimento/Cobranca/ImovelEstoqueEditar.xhtml";
 	}
-	
-	
+		
 	public void consultaEstoque() {
 		ContratoCobrancaDao contratoCobrancaDao = new ContratoCobrancaDao();
 		listaConsultaEstoque = contratoCobrancaDao.consultaImovelEstoque();
 	}
 	
+	public BigDecimal calcularVariacaoCustos(BigDecimal valorLeilao2, BigDecimal valorEmprestimo) {
+	    if (valorLeilao2 != null && valorEmprestimo != null && BigDecimal.ZERO.compareTo(valorEmprestimo) != 0) {
+	        return (valorLeilao2.divide(valorEmprestimo, RoundingMode.HALF_UP).subtract(BigDecimal.ONE)).multiply(BigDecimal.valueOf(100));
+	    } else {
+	        // Trata o caso em que um dos valores é nulo ou zero
+	        return BigDecimal.ZERO;
+	    }
+	}
+
+	public BigDecimal calcularLtvLeilao(BigDecimal valorLeilao2, BigDecimal valorMercado) {
+	    if (valorLeilao2 != null && valorMercado != null && BigDecimal.ZERO.compareTo(valorMercado) != 0) {
+	        return (valorLeilao2.divide(valorMercado, RoundingMode.HALF_UP)).multiply(BigDecimal.valueOf(100));
+	    } else {
+	        // Trata o caso em que um dos valores é nulo ou zero
+	        return BigDecimal.ZERO; // Ou outro valor padrão, dependendo do seu caso
+	    }
+	}
+    
 	public void readXLSXFileRelatorioEstoque() throws IOException {
 
-		XSSFWorkbook wb = new XSSFWorkbook(getClass().getResourceAsStream("/resource/TabelaVazia.xlsx"));
+	    XSSFWorkbook wb = new XSSFWorkbook(getClass().getResourceAsStream("/resource/TabelaVazia.xlsx"));
+	    XSSFSheet sheet = wb.getSheetAt(0);
 
-		XSSFSheet sheet = wb.getSheetAt(0);
-		
-		ImovelEstoqueDao imovelEstoqueDao = new ImovelEstoqueDao();
-		List<RelatorioEstoque> listRelatorioEstoque = imovelEstoqueDao.listRelatorioEstoque();
-		
-		// Cria uma instância de XSSFCellStyle para aplicar formatação ao cabeçalho
-		XSSFCellStyle headerCellStyle = wb.createCellStyle();
+	    ImovelEstoqueDao imovelEstoqueDao = new ImovelEstoqueDao();
+	    List<RelatorioEstoque> listRelatorioEstoque = imovelEstoqueDao.listRelatorioEstoque();
 
-		// Define a cor de fundo para o cabeçalho
-		headerCellStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
-		headerCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+	    // Cria uma instância de XSSFCellStyle para aplicar formatação ao cabeçalho
+	    XSSFCellStyle headerCellStyle = wb.createCellStyle();
 
-		// Cria uma fonte para o texto do cabeçalho
-		XSSFFont font = wb.createFont();
-		font.setBold(true); // Defina o texto em negrito
-		headerCellStyle.setFont(font);
+	    // Define a cor de fundo para o cabeçalho
+	    headerCellStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+	    headerCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
-		// Alinha o texto ao centro horizontalmente e verticalmente
-		headerCellStyle.setAlignment(HorizontalAlignment.CENTER);
-		headerCellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-		
-		// Crie o cabeçalho e aplique o estilo a cada célula do cabeçalho
-		XSSFRow headerRow = sheet.createRow(0);
-		String[] headerTexts = {
-		    "N° CONTRATO", "VARIAÇÃO CUSTOS ATÉ LEILÃO", "LTV DO LEILÃO", "VALOR DO EMPRÉSTIMO",
-		    "VENDA FORÇADA", "VALOR MERCADO", "CLIENTE", "MATRÍCULA", "IMÓVEL", "CONSOLIDADO EM",
-		    "1º LEILÃO", "2º LEILÃO", "LEILÃO ESTOQUE", "STATUS LEILÃO", "STATUS ATUAL",
-		    "VALOR 2º LEILÃO", "VALOR VENDA", "DATA VENDA", "TIPO VENDA"
-		};
+	    // Cria uma fonte para o texto do cabeçalho
+	    XSSFFont font = wb.createFont();
+	    font.setBold(true); // Defina o texto em negrito
+	    headerCellStyle.setFont(font);
 
-		for (int i = 0; i < headerTexts.length; i++) {
-		    XSSFCell cell = headerRow.createCell(i);
-		    cell.setCellValue(headerTexts[i]);
-		    cell.setCellStyle(headerCellStyle);
-		}
-		
-		// Ajusta automaticamente o tamanho das colunas com base no conteúdo
-		for (int i = 0; i < headerTexts.length; i++) {
-		    sheet.autoSizeColumn(i);
-		}
-		
-		// Cria uma instância de XSSFCellStyle para a formatação das linhas pares
-		XSSFCellStyle evenRowStyle = wb.createCellStyle();
+	    // Alinha o texto ao centro horizontalmente e verticalmente
+	    headerCellStyle.setAlignment(HorizontalAlignment.CENTER);
+	    headerCellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+	    
+	    
+	    // Crie o cabeçalho e aplique o estilo a cada célula do cabeçalho
+	    XSSFRow headerRow = sheet.createRow(0);
+	    String[] headerTexts = {
+	        "N° CONTRATO", "VARIAÇÃO CUSTOS ATÉ LEILÃO", "LTV DO LEILÃO", "VALOR DO EMPRÉSTIMO",
+	        "VENDA FORÇADA", "VALOR MERCADO", "CLIENTE", "MATRÍCULA", "IMÓVEL", "CONSOLIDADO EM",
+	        "1º LEILÃO", "2º LEILÃO", "LEILÃO ESTOQUE", "STATUS LEILÃO", "STATUS ATUAL",
+	        "VALOR 2º LEILÃO", "VALOR VENDA", "DATA VENDA", "TIPO VENDA"
+	    };
 
-		// Define a cor de fundo para as linhas pares
-		//evenRowStyle.setFillForegroundColor(IndexedColors.LIGHT_GREEN.getIndex());
-		//evenRowStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+	    for (int i = 0; i < headerTexts.length; i++) {
+	        XSSFCell cell = headerRow.createCell(i);
+	        cell.setCellValue(headerTexts[i]);
+	        cell.setCellStyle(headerCellStyle);
+	    }
 
-		// Cria uma instância de XSSFCellStyle para a formatação das linhas ímpares
-		XSSFCellStyle oddRowStyle = wb.createCellStyle();
+	    // Ajusta automaticamente o tamanho das colunas com base no conteúdo
+	    for (int i = 0; i < headerTexts.length; i++) {
+	        sheet.autoSizeColumn(i);
+	    }
 
-		// Define a cor de fundo para as linhas ímpares
-		oddRowStyle.setFillForegroundColor(IndexedColors.LEMON_CHIFFON.getIndex());
-		oddRowStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+	    // Cria uma instância de XSSFCellStyle para a formatação das linhas pares
+	    XSSFCellStyle evenRowStyle = wb.createCellStyle();
 
-		// Cria uma fonte para os estilos das linhas
-		XSSFFont rowFont = wb.createFont();
-		rowFont.setBold(false); // Desligue o negrito
-		evenRowStyle.setFont(rowFont);
-		oddRowStyle.setFont(rowFont);
+	    // Define a cor de fundo para as linhas pares
+	    //evenRowStyle.setFillForegroundColor(IndexedColors.LIGHT_GREEN.getIndex());
+	    //evenRowStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
-		// Inicia um contador para rastrear linhas ímpares/pares
-		int rowNum = 0;
+	    // Cria uma instância de XSSFCellStyle para a formatação das linhas ímpares
+	    XSSFCellStyle oddRowStyle = wb.createCellStyle();
 
-		for (RelatorioEstoque relatorio : listRelatorioEstoque) {
-		    XSSFRow linha = sheet.createRow(rowNum + 1); // Comece a partir da segunda linha (0 é o cabeçalho)
+	    // Define a cor de fundo para as linhas ímpares
+	    oddRowStyle.setFillForegroundColor(IndexedColors.LEMON_CHIFFON.getIndex());
+	    oddRowStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
-		    // Aplicar o estilo alternado com base no número da linha
-		    XSSFCellStyle rowStyle = (rowNum % 2 == 0) ? evenRowStyle : oddRowStyle;
-		    for (int i = 0; i < headerTexts.length; i++) {
-		        XSSFCell cell = linha.createCell(i);
-		        // Defina o valor da célula com base nos dados do relatório
-		        // ...
-		        cell.setCellStyle(rowStyle);
-		    }
-
-		    rowNum++;
-		}
-		
-
-		int iLinha = 0;
-
-		XSSFRow linha = sheet.getRow(iLinha);
-		if (linha == null) {
-			sheet.createRow(iLinha);
-			linha = sheet.getRow(iLinha);
-		}
-
-		gravaCelula(0, "N° CONTRATO", linha);
-		gravaCelula(1, "VARIAÇÃO CUSTOS ATÉ LEILÃO", linha);
-		gravaCelula(2, "LTV DO LEILÃO", linha);
-		gravaCelula(3, "VALOR DO EMPRESTIMO", linha);
-		gravaCelula(4, "VENDA FORÇADA", linha);
-		gravaCelula(5, "VALOR MERCADO", linha);
-		gravaCelula(6, "CLIENTE", linha);
-		gravaCelula(7, "MATRÍCULA", linha);
-		gravaCelula(8, "IMÓVEL", linha);
-		gravaCelula(9, "CONSOLIDADO EM", linha);
-		gravaCelula(10, "1º LEILÃO", linha);
-		gravaCelula(11, "2º LEILÃO", linha);
-		gravaCelula(12, "LEILÃO ESTOQUE", linha);
-		gravaCelula(13, "STATUS LEILÃO", linha);
-		gravaCelula(14, "STATUS ATUAL", linha);
-		gravaCelula(15, "VALOR 2º LEILÃO", linha);
-		gravaCelula(16, "VALOR VENDA", linha);
-		gravaCelula(17, "DATA VENDA", linha);
-		gravaCelula(18, "TIPO VENDA", linha);
+	    // Cria uma fonte para os estilos das linhas
+	    XSSFFont rowFont = wb.createFont();
+	    rowFont.setBold(false); // Desligue o negrito
+	    evenRowStyle.setFont(rowFont);
+	    oddRowStyle.setFont(rowFont);
+	    
+	    // Cria um estilo personalizado para formato de moeda
+	    XSSFCellStyle currencyCellStyle = wb.createCellStyle();
+	    //currencyCellStyle.setDataFormat(wb.createDataFormat().getFormat("R$ #,##0.00")); // Define o formato da moeda
+	    
+	    CreationHelper ch = wb.getCreationHelper();
+	    currencyCellStyle.setDataFormat(ch.createDataFormat().getFormat("_(R$* #,##0.00_);_(R$* (#,##0.00);_(R$* \"-\"??_);_(@_)")); // Define o formato da moeda
 
 
-		iLinha++;
-		
-		for (int iRelatorio = 0; iRelatorio < listRelatorioEstoque.size(); iRelatorio++) {
-			RelatorioEstoque relatorio = listRelatorioEstoque.get(iRelatorio);
+	    // Inicia um contador para rastrear linhas ímpares/pares
+	    int rowNum = 0;
 
-			linha = sheet.getRow(iLinha);
-			if (linha == null) {
-				sheet.createRow(iLinha);
-				linha = sheet.getRow(iLinha);
-			}
+	    for (RelatorioEstoque relatorio : listRelatorioEstoque) {
+	        XSSFRow linha = sheet.createRow(rowNum + 1); // Comece a partir da segunda linha (0 é o cabeçalho)
 
-			gravaCelula(0, relatorio.getNumeroContratoRelatorio(), linha);
-			gravaCelula(1, relatorio.getVariacaoCustoRelatorio(), linha);
-			gravaCelula(2, relatorio.getLtvLeilaoRelatorio(), linha);
-			gravaCelula(3, relatorio.getValorEmprestimoRelatorio(), linha);
-			gravaCelula(4, relatorio.getVendaForcadaRelatorio(), linha);
-			gravaCelula(5, relatorio.getValorMercadoRelatorio(), linha);
-			gravaCelula(6, relatorio.getNomePagadorRelatorio(), linha);
-			gravaCelula(7, relatorio.getNumeroMatriculaRelatorio(), linha);
-			gravaCelula(8, relatorio.getEnderecoCompletoRelatorio(), linha);
-			gravaCelula(9, relatorio.getDataConsolidadoRelatorio(), linha);
-			gravaCelula(10, relatorio.getDataLeilao1Relatorio(), linha);
-			gravaCelula(11, relatorio.getDataLeilao2Relatorio(), linha);
-			gravaCelula(12, relatorio.getDataLeilao3Relatorio(), linha);
-			gravaCelula(13, relatorio.getStatusLeilaoRelatorio(), linha);
-			gravaCelula(14, relatorio.getStatusAtualRelatorio(), linha);
-			gravaCelula(15, relatorio.getValorLeilao2Relatorio(), linha);
-			gravaCelula(16, relatorio.getValorVendaRelatorio(), linha);
-			gravaCelula(17, relatorio.getDataVendaRelatorio(), linha);
-			gravaCelula(18, relatorio.getTipoVendaRelatorio(), linha);
+	        // Aplicar o estilo alternado com base no número da linha
+	        XSSFCellStyle rowStyle = (rowNum % 2 == 0) ? evenRowStyle : oddRowStyle;
+	        for (int i = 0; i < headerTexts.length; i++) {
+	            XSSFCell cell = linha.createCell(i);
+	            // Defina o valor da célula com base nos dados do relatório
+	            // ...
+	            cell.setCellStyle(rowStyle);
+	        }
 
+	        rowNum++;
+	    }
 
-			iLinha++;
-		}
+	    int iLinha = 0;
 
-		ByteArrayOutputStream fileOut = new ByteArrayOutputStream();
-		// escrever tudo o que foi feito no arquivo
-		wb.write(fileOut);
+	    XSSFRow linha = sheet.getRow(iLinha);
+	    if (linha == null) {
+	        sheet.createRow(iLinha);
+	        linha = sheet.getRow(iLinha);
+	    }
 
-		// fecha a escrita de dados nessa planilha
-		wb.close();
+	    gravaCelula(0, "N° CONTRATO", linha);
+	    gravaCelula(1, "VARIAÇÃO CUSTOS ATÉ LEILÃO", linha);
+	    gravaCelula(2, "LTV DO LEILÃO", linha);
+	    gravaCelula(3, "VALOR DO EMPRESTIMO", linha);
+	    gravaCelula(4, "VENDA FORÇADA", linha);
+	    gravaCelula(5, "VALOR MERCADO", linha);
+	    gravaCelula(6, "CLIENTE", linha);
+	    gravaCelula(7, "MATRÍCULA", linha);
+	    gravaCelula(8, "IMÓVEL", linha);
+	    gravaCelula(9, "CONSOLIDADO EM", linha);
+	    gravaCelula(10, "1º LEILÃO", linha);
+	    gravaCelula(11, "2º LEILÃO", linha);
+	    gravaCelula(12, "LEILÃO ESTOQUE", linha);
+	    gravaCelula(13, "STATUS LEILÃO", linha);
+	    gravaCelula(14, "STATUS ATUAL", linha);
+	    gravaCelula(15, "VALOR 2º LEILÃO", linha);
+	    gravaCelula(16, "VALOR VENDA", linha);
+	    gravaCelula(17, "DATA VENDA", linha);
+	    gravaCelula(18, "TIPO VENDA", linha);
 
-		final GeradorRelatorioDownloadCliente gerador = new GeradorRelatorioDownloadCliente(
-				FacesContext.getCurrentInstance());
+	    iLinha++;
 
-		gerador.open(String.format("Galleria Bank - Estoque %s.xlsx", ""));
-		gerador.feed(new ByteArrayInputStream(fileOut.toByteArray()));
-		gerador.close();
-		
+	    for (int iRelatorio = 0; iRelatorio < listRelatorioEstoque.size(); iRelatorio++) {
+	        RelatorioEstoque relatorio = listRelatorioEstoque.get(iRelatorio);
 
+	        linha = sheet.getRow(iLinha);
+	        if (linha == null) {
+	            sheet.createRow(iLinha);
+	            linha = sheet.getRow(iLinha);
+	        }
+
+	        gravaCelula(0, relatorio.getNumeroContratoRelatorio(), linha);
+	        gravaCelula(1, relatorio.getVariacaoCustoRelatorio(), linha);
+	        gravaCelula(2, relatorio.getLtvLeilaoRelatorio(), linha);
+	        gravaCelulaComEstiloMoeda(3, relatorio.getValorEmprestimoRelatorio(), linha, currencyCellStyle);
+	        gravaCelulaComEstiloMoeda(4, relatorio.getVendaForcadaRelatorio(), linha, currencyCellStyle);
+	        gravaCelulaComEstiloMoeda(5, relatorio.getValorMercadoRelatorio(), linha, currencyCellStyle);
+	        gravaCelula(6, relatorio.getNomePagadorRelatorio(), linha);
+	        gravaCelula(7, relatorio.getNumeroMatriculaRelatorio(), linha);
+	        gravaCelula(8, relatorio.getEnderecoCompletoRelatorio(), linha);
+	        gravaCelula(9, relatorio.getDataConsolidadoRelatorio(), linha);
+	        gravaCelula(10, relatorio.getDataLeilao1Relatorio(), linha);
+	        gravaCelula(11, relatorio.getDataLeilao2Relatorio(), linha);
+	        gravaCelula(12, relatorio.getDataLeilao3Relatorio(), linha);
+	        gravaCelula(13, relatorio.getStatusLeilaoRelatorio(), linha);
+	        gravaCelula(14, relatorio.getStatusAtualRelatorio(), linha);
+	        gravaCelulaComEstiloMoeda(15, relatorio.getValorLeilao2Relatorio(), linha, currencyCellStyle);
+	        gravaCelulaComEstiloMoeda(16, relatorio.getValorVendaRelatorio(), linha, currencyCellStyle);
+	        gravaCelula(17, relatorio.getDataVendaRelatorio(), linha);
+	        gravaCelula(18, relatorio.getTipoVendaRelatorio(), linha);
+
+	        // Aplica o estilo de formatação da linha
+	        XSSFCellStyle rowStyle = (iLinha % 2 == 0) ? evenRowStyle : oddRowStyle;
+	        for (int i = 0; i < headerTexts.length; i++) {
+	            XSSFCell cell = linha.getCell(i);
+	            cell.setCellStyle(rowStyle);
+	        }
+
+	        rowNum++;
+
+	        iLinha++;
+	    }
+
+	    ByteArrayOutputStream fileOut = new ByteArrayOutputStream();
+	    // escrever tudo o que foi feito no arquivo
+	    wb.write(fileOut);
+
+	    // fecha a escrita de dados nessa planilha
+	    wb.close();
+
+	    final GeradorRelatorioDownloadCliente gerador = new GeradorRelatorioDownloadCliente(
+	            FacesContext.getCurrentInstance());
+
+	    gerador.open(String.format("Galleria Bank - Estoque %s.xlsx", ""));
+	    gerador.feed(new ByteArrayInputStream(fileOut.toByteArray()));
+	    gerador.close();
 	}
+	
+	private void gravaCelulaComEstiloMoeda(int columnIndex, BigDecimal valor, XSSFRow linha, XSSFCellStyle style) {
+	    XSSFCell cell = linha.createCell(columnIndex);
+
+	    if (valor != null) {
+	        cell.setCellValue(valor.doubleValue()); // Converte o BigDecimal para double antes de definir o valor
+	    } else {
+	        cell.setCellValue(0.0);
+	    }
+
+	    cell.setCellStyle(style);
+	}
+	
+	// Função para obter o nome da coluna do Excel com base no índice (ex: 0 -> A, 1 -> B, ...)
+	private String getColunaExcel(int coluna) {
+	    int div = coluna;
+	    String colunaExcel = "";
+	    int modulo;
+
+	    while (div > 0) {
+	        modulo = (div - 1) % 26;
+	        colunaExcel = (char) (65 + modulo) + colunaExcel;
+	        div = (int) ((div - modulo) / 26);
+	    }
+	    return colunaExcel;
+	}
+	
 	private void gravaCelula(Integer celula, String value, XSSFRow linha) {
 		if (linha.getCell(celula) == null)
 			linha.createCell(celula);
