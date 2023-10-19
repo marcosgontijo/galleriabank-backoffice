6178b643c75e66c5dc89c59b0b86d1278b4eaa96
Lucaslamr <lucaslamr@gmail.com>
2023-10-19 12:07:51 -0300

    att incluir pessoas

--- galleriafinancas/src/com/webnowbr/siscoat/cobranca/mb/ContratoCobrancaMB.java
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/mb/ContratoCobrancaMB.java
@@ -31176,13 +31176,13 @@ public class ContratoCobrancaMB {
 				return CommonsUtil.castAsLong(one.getId()).compareTo(other.getId());
 			}
 		});
-
 	}
 
 	public void preparaAdicionarPessoaAnalise() {
 		this.documentoAnaliseAdicionar = new DocumentoAnalise();
 		documentoAnaliseAdicionar.setPagador(new PagadorRecebedor());
 		documentoAnaliseAdicionar.setContratoCobranca(this.objetoContratoCobranca);
+		documentoAnaliseAdicionar.setObservacao(".");
 	}
 
 	public void adicionarPessoaAnalise() {
