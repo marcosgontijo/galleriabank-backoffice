c6e0f39a6ca90decfeeb9be19d5370fff4bfdf9c
Hermes Jr <webnowbr@gmail.com>
2024-03-12 09:25:19 -0300

    Atualizacoes

--- galleriafinancas/src/com/webnowbr/siscoat/cobranca/mb/ContratoCobrancaMB.java
+++ galleriafinancas/src/com/webnowbr/siscoat/cobranca/mb/ContratoCobrancaMB.java
@@ -23756,7 +23756,7 @@ public class ContratoCobrancaMB {
 					contratoCobrancaDetalhesParcial.setDataVencimento(parcelasBoleto.getDataVencimento());
 					contratoCobrancaDetalhesParcial.setVlrParcela(parcelasBoleto.getVlrParcelaAtualizada());
 					contratoCobrancaDetalhesParcial.setDataPagamentoGalleria(dataPagamento.getTime());
-					contratoCobrancaDetalhesParcial.setVlrRecebido(parcelasBoleto.getVlrBoletoKobana());
+					contratoCobrancaDetalhesParcial.setVlrRecebido(this.vlrRecebido);
 
 					// TODO SOMAR BAIXAS PARCIAIS
 					for (QuitacaoParcelasPDF parcelaPresente : this.quitacaoPDF.getParcelas()) {
@@ -23765,7 +23765,7 @@ public class ContratoCobrancaMB {
 							BigDecimal totalBaixas = getTotalParcelasBaixadas(
 									parcelasBoleto.getListContratoCobrancaDetalhesParcial());
 							// adiciona valor baixado do boleto
-							totalBaixas = totalBaixas.add(parcelasBoleto.getVlrBoletoKobana());
+							totalBaixas = totalBaixas.add(this.vlrRecebido);
 							// se não considerar Tx Adm descontar valor
 							BigDecimal valorParcelaPresente = BigDecimal.ZERO;
 							valorParcelaPresente = parcelaPresente.getValorPresenteParcela();
